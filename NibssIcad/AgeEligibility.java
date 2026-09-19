469039d04f2672eb50e0285306634b084c1956e3
kelvin Esho <kelvinesho@gmail.com>
2026-09-19 19:02:06 +0100

    re factored

--- NibssIcad/AgeEligibility.java
+++ NibssIcad/AgeEligibility.java
@@ -11,7 +11,7 @@ public class AgeEligibility {
         age = input.nextInt();
         if (age < 18){
             System.out.println("INELIGIBLE! \n Age: " + age);
-            System.out.println("Try Again when you clock 18years!")
+            System.out.println("Try Again when you clock 18years!");
         }else {
             System.out.println("ELIGIBLE \n Age: " + age);
             System.out.println("Congratulations");
